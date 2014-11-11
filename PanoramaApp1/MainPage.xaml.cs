e3b01fe8acc5025d220bfff3c6ca7d1383a8a76e
Kunaal Jain <kunaalus@gmail.com>
2014-11-11 17:49:48 +0530

    Initial commit
    With many improvements.

---
+++ PanoramaApp1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PanoramaApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void Panorama_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
